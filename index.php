3bb3ffae41c50c7e788ad6dc10fc756dbbe34287
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-12-06 23:07:56 +0800

    1206 改正調整

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $isbn = $_GET["isbn"];
 //$isbn = "http://jenda.lib.nccu.edu.tw/search~S5*cht?/dWORD%28{u96FB}{u8166}{u7A0B}{u5F0F}%29/dword+{215f55}{215365}{214f35}{213d39}/-3%2C-1%2C0%2CB/frameset&FF=dwordless+picture+book&1%2C1%2C#.VmQzEbiGSko";
 //$isbn = "http://jenda.lib.nccu.edu.tw/search~S5*cht?/dCommunism+--+Social+aspects+--+Soviet+Union+--+Hi/dcommunism+social+aspects+soviet+union+history/-3%2C-1%2C0%2CB/frameset&FF=dcommunism+soviet+union+addresses+essays+lectures&1%2C1%2C#.VmQzmbiGSko";
 //$isbn = "http://jenda.lib.nccu.edu.tw/search~S5*cht?/XWord+2013&SORT=D/XWord+2013&SORT=D&SUBKEY=Word+2013/1%2C42%2C42%2CB/frameset&FF=XWord+2013&SORT=D&1%2C1%2C#.VmQyCLiGSko";
-$isbn = "http://jenda.lib.nccu.edu.tw/search*cht/X?SEARCH=Soviet+communism+and+the+socialist+vision#.VmREzLiGSko";
+//$isbn = "http://jenda.lib.nccu.edu.tw/search*cht/X?SEARCH=Soviet+communism+and+the+socialist+vision#.VmREzLiGSko";
 //echo $isbn;
 // ---------------------
 // 先取得快取
